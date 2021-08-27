11350a73fa4875f6ce04bb2f252f8dbee477e51c
Júlia Suriani <89102778+JuliaSuriani@users.noreply.github.com>
2021-08-27 18:42:19 -0300

    Create URI1183-C.C

---
+++ Exercicios em C/URI1183-C.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+double a=0.0, M[12][12];
+char T[2];
+int C,X,Y,Z,p=1;
+scanf("%s", &T);
+    for(X=0;X<=11;X++)
+    {
+        for(Y=0; Y<=11; Y++)
+            scanf("%lf", &M[X][Y]);
+    }
+    for(Z=0; Z<=11;Z++)
+    {
+        for(C=p; C<=11;C++)
+            a+=M[Z][C];
+        p++;
+    }
+    if(T[0]=='S')
+        printf("%.1lf\n",a);
+    else if(T[0]=='M')
+    {
+        a=a/66.0;
+        printf("%.1lf\n",a);
+    }
+    return 0;
+
+}
