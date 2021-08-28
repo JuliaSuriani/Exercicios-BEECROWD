93e16e1f3e81c7a46d856f52f4adbea31f5a5044
Júlia Suriani <89102778+JuliaSuriani@users.noreply.github.com>
2021-08-28 14:52:25 -0300

    Add files via upload

---
+++ Python/URI1049-Python.py
@@ -0,0 +1,20 @@
+# Exercicio 1049 de Python do URI author:Julia Suriani
+v1 = input()
+v2 = input()
+v3 = input()
+if v1 == "vertebrado" and v2 == "ave" and v3 == "carnivoro":
+    print("aguia")
+if v1 == "vertebrado" and v2 == "ave" and v3 == "onivoro":
+    print("pomba")
+if v1 == "vertebrado" and v2 == "mamifero" and v3 == "onivoro":
+    print("homem")
+if v1 == "vertebrado" and v2 == "mamifero" and v3 == "herbivoro":
+    print("vaca")
+if v1 == "invertebrado" and v2 == "inseto" and v3 == "hematofago":
+    print("pulga")
+if v1 == "invertebrado" and v2 == "inseto" and v3 == "herbivoro":
+    print("lagarta")
+if v1 == "invertebrado" and v2 == "anelideo" and v3 == "hematofago":
+    print("sanguessuga")
+if v1 == "invertebrado" and v2 == "anelideo" and v3 == "onivoro":
+    print("minhoca")
